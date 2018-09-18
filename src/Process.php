0415b4cc72abcb0347130af8ed87e46ffd989352
Oliver Watts <ollie@teknicode.uk>
2018-09-18 16:26:36 +0100

    send form by email

--- src/Process.php
+++ src/Process.php
@@ -30,9 +30,13 @@ class Process{
     if( $_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST) ){
       if( is_array($this->attributes) && !empty($this->attributes) ){
         $this->compile();
+        return $this->transmit();
       }else{
         //output error
-        echo "Attributes missing";
+        return [
+          "status" => "failed",
+          "error" => "Required Attributes Missing"
+        ];
       }
     }
   }
@@ -43,7 +47,6 @@ class Process{
     foreach( $_POST as $k => $v ){
       $this->compiled .= "<p><b>$k</b><br/>$v</p>";
     }
-    $this->transmit();
   }
 
   private function transmit(){
@@ -51,26 +54,46 @@ class Process{
     switch( $this->method ){
       case "mail":
         $mail = new \PHPMailer\PHPMailer\PHPMailer(true);
-        if( is_array($this->get("smtp")) ){
-          $config = $this->get("smtp");
-          //load smtp
-          $mail->SMTPDebug = 0;                                 // Enable verbose debug output
-          $mail->isSMTP();                                      // Set mailer to use SMTP
-          $mail->Host = $config['Host'];  // Specify main and backup SMTP servers
-          $mail->SMTPAuth = true;                               // Enable SMTP authentication
-          $mail->Username = $config['Username'];                 // SMTP username
-          $mail->Password = $config['Password'];                           // SMTP password
-          $mail->SMTPSecure = (isset($config['SSL'])?$config['SSL']:'tls');                            // Enable TLS encryption, `ssl` also accepted
-          $mail->Port = $config['Port'];
+        try{
+          if( is_array($this->get("smtp")) ){
+            $config = $this->get("smtp");
+            //load smtp
+            $mail->SMTPDebug = 0;                                 // Enable verbose debug output
+            $mail->isSMTP();                                      // Set mailer to use SMTP
+            $mail->Host = $config['Host'];  // Specify main and backup SMTP servers
+            $mail->SMTPAuth = true;                               // Enable SMTP authentication
+            $mail->Username = $config['Username'];                 // SMTP username
+            $mail->Password = $config['Password'];                           // SMTP password
+            $mail->SMTPSecure = (isset($config['SSL'])?$config['SSL']:'tls');                            // Enable TLS encryption, `ssl` also accepted
+            $mail->Port = $config['Port'];
+          }
+
+          if( !empty($this->get("from")) ){
+            $mail->setFrom($this->get("from")['address'],$this->get("from")['name']);
+          }
+          if( is_array($this->get("recipient")) ){
+            foreach( $this->get("recipient") as $r ){
+                $mail->addAddress( $r );
+            }
+          }else{
+            $mail->addAddress( $this->get("recipient") );
+          }
+          $mail->isHTML(true);
+          $mail->Subject = ( !empty($this->get("subject")) ? $this->get("subject") : 'Web Form Message');
+          $mail->Body    = $this->compiled;
+          $mail->AltBody = strip_tags(str_replace(["<br/>","<br>"],"\n",$this->compiled));
+          $mail->send();
+
+          return [
+            "status" => "success"
+          ];
+        }catch(\PHPMailer\PHPMailer\Exception $e){
+          return [
+            "status" => "failed",
+            "error" => $e->errorMessage()
+          ];
         }
 
-        $mail->setFrom('ollie@teknicode.uk', 'Ollie');
-        $mail->addAddress( $this->get("recipient") );
-        $mail->isHTML(true);
-        $mail->Subject = 'Here is the subject';
-        $mail->Body    = $this->compiled;
-        $mail->AltBody = strip_tags(str_replace(["<br/>","<br>"],"\n",$this->compiled));
-        $mail->send();
       break;
 
       case "sms":
